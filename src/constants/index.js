800554e3056da64bf49892916af93e22ed124c59
Harish <91340729+Battarasetty@users.noreply.github.com>
2026-01-13 17:50:14 +0530

    Modify experience icon and clean up testimonials
    Updated experience icon and removed testimonials and commented-out projects.

--- src/constants/index.js
+++ src/constants/index.js
@@ -81,7 +81,7 @@ const experiences = [
    {
     title: "Software Engineer – Frontend",
     company_name: "Entitled Arts Pvt Ltd",
-    icon: meta, // replace with actual logo if available
+    icon: ETiled,
     iconBg: "#1E293B",
     date: "Jan 2024 – Present",
     points: [
@@ -107,33 +107,6 @@ const experiences = [
   },
 ];
 
-const testimonials = [
-    {
-        testimonial:
-            "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-        name: "Sara Lee",
-        designation: "CFO",
-        company: "Acme Co",
-        image: "https://randomuser.me/api/portraits/women/4.jpg",
-    },
-    {
-        testimonial:
-            "I've never met a web developer who truly cares about their clients' success like Rick does.",
-        name: "Chris Brown",
-        designation: "COO",
-        company: "DEF Corp",
-        image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-        testimonial:
-            "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-        name: "Lisa Wang",
-        designation: "CTO",
-        company: "456 Enterprises",
-        image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
-];
-
 const projects = [
     {
         name: "Housing Estate",
@@ -156,27 +129,6 @@ const projects = [
         image: HousingEstate,
         source_code_link: "https://github.com/",
     },
-    // {
-    //     name: "Job IT",
-    //     description:
-    //         "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
-    //     tags: [
-    //         {
-    //             name: "react",
-    //             color: "blue-text-gradient",
-    //         },
-    //         {
-    //             name: "restapi",
-    //             color: "green-text-gradient",
-    //         },
-    //         {
-    //             name: "scss",
-    //             color: "pink-text-gradient",
-    //         },
-    //     ],
-    //     image: jobit,
-    //     source_code_link: "https://github.com/",
-    // },
     {
         name: "Youtube Application",
         description:
